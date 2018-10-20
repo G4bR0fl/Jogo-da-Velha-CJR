e1f5c019fb0c900aa1bf32c443823a62165f6a5f
​Gustavo Luigi <gustavolusilo@gmail.com>
2018-10-20 00:50:10 -0300

    Adicionada matriz base para teste de ganho do jogo da velha

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
+//--------------------VARIABLES--------------------//
+
+let matrix =   [['a', 'a', 'a'],
+                ['a', 'a', 'a'],
+                ['a', 'a', 'a']];
+
+//------------------END_VARIABLES------------------//
+
+//--------------------LISTENERS--------------------//
+
 document.getElementById("btn_reset").addEventListener("click", reset);
+
+//------------------END_LISTENERS------------------//
+
+//--------------------FUNCTIONS--------------------//
 
 function reset() {
     
 }
+
+//------------------END_FUNCTIONS------------------//

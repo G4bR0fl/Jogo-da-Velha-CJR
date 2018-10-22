e8b1381562aaed6f89c75b968818350d5ba0cdd2
GabrielBessa <gabrielbessarofl@gmail.com>
2018-10-22 11:09:21 -0300

    is_game_finished() done

--- js/index.js
+++ js/index.js
@@ -16,14 +16,16 @@ function create_matrix(){
 function is_game_finished(game_matrix){
   var check_win_X = [false, false, false];
   var check_win_O = [false, false, false];
-
+  var counter = 0; 
   var game_result = false;
+  var return_variables = [];
   // Checking Rows 
   for(var i = 0; i < 3; i++){
     for(var j = 0; j < 3; j++){
       // In case matrix has 'O'
       if (game_matrix[i][j] == 'O'){
         check_win_O[j] = true;
+        counter++;
         if (check_win_O[0] == true && check_win_O[1] == true && check_win_O[2] == true) {
           console.log("O has won");
           return game_result = true;
@@ -32,6 +34,7 @@ function is_game_finished(game_matrix){
       // In case matrix has 'X'
       if(game_matrix[i][j] == 'X'){
         check_win_X[j] = true;
+        counter++;
         if (check_win_X[0] == true && check_win_X[1] == true && check_win_X[2] == true) {
           console.log("X has won");
           return game_result = true;
@@ -52,6 +55,7 @@ function is_game_finished(game_matrix){
       // In case matrix has 'O'
       if (game_matrix[i][j] == 'O') {
         check_win_O[i] = true;
+        counter++;
         if (check_win_O[0] == true && check_win_O[1] == true && check_win_O[2] == true) {
           console.log("O has won");
           return game_result = true;
@@ -59,7 +63,8 @@ function is_game_finished(game_matrix){
       }
       // In case matrix has 'X'
       if (game_matrix[i][j] == 'X') {
-        check_win_X[i] = true; 
+        check_win_X[i] = true;
+        counter++; 
         if (check_win_X[0] == true && check_win_X[1] == true && check_win_X[2] == true) {
           console.log("X has won");
           return game_result = true;
@@ -101,5 +106,8 @@ function is_game_finished(game_matrix){
       }
     }
   }
-  return game_result;
+  return_variables[0] = game_result;
+  return_variables[1] = counter; // Counter passes 2 times in the matrix, that's why it goes 2 by 2.
+  return [game_result, counter/2]; // So it's divided by 2.
 }
+
